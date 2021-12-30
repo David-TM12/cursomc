81c27f910c7d46c909d550df9b9010be3aa4575c
DavidTM <deivi_gf144@hotmail.com>
2021-12-30 15:54:38 -0400

    Pedido, EstadoPagamento e Pagamento - Parte 1

--- src/main/java/com/davidmagalhaes/cursomc/domain/Cliente.java
+++ src/main/java/com/davidmagalhaes/cursomc/domain/Cliente.java
@@ -37,6 +37,8 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 		
 	}
@@ -105,6 +107,14 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}	
 
 	@Override
 	public int hashCode() {
@@ -129,8 +139,6 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
-	
-	
+	}
 
 }
---
+++ src/main/java/com/davidmagalhaes/cursomc/domain/enums/EstadoPagamento.java
@@ -0,0 +1,39 @@
+package com.davidmagalhaes.cursomc.domain.enums;
+
+public enum EstadoPagamento {
+	
+	PENDENTE(1, "Pendente"),
+	QUITADO(2, "Quitado"),
+	CANCELADO(3, "Cancelado");
+	
+	private int cod;
+	private String descricao;
+	
+	private EstadoPagamento(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return this.cod;
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	
+	public static EstadoPagamento toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(EstadoPagamento x : EstadoPagamento.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
