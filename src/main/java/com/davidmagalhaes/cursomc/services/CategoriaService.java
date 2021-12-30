52c2e4ca779ab15025db88db49f9397f4e933db2
David Magalhães <deivi_gf144@hotmail.com>
2021-12-29 22:41:47 -0400

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/davidmagalhaes/cursomc/services/CategoriaService.java
+++ src/main/java/com/davidmagalhaes/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.davidmagalhaes.cursomc.domain.Categoria;
 import com.davidmagalhaes.cursomc.repositories.CategoriaRepository;
+import com.davidmagalhaes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,6 +18,7 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado Id: "+ id +", Tipo: " + Categoria.class.getName()));
 	}
 }
---
+++ src/main/java/com/davidmagalhaes/cursomc/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.davidmagalhaes.cursomc.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException(String msg, Throwable causes) {
+		super(msg, causes);
+	}
+	
+}
